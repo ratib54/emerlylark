05186caeca1575afda2b278da550da96e63320c5
FarhanZizz <iamfarhanaziz@gmail.com>
2022-12-10 18:04:30 +0530

    projects section complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home';
-import Navbar from './Components/Navbar';
 
 function App() {
   const router = createBrowserRouter([
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from './Hero';
 import Navbar from './Navbar';
+import Projects from './Projects';
 
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Hero></Hero>
+            <Projects></Projects>
         </div>
     );
 };
