de96584104432b21d113b59b8781f28d6df63812
FarhanZizz <iamfarhanaziz@gmail.com>
2022-12-10 13:53:24 +0530

    navbar finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home';
+import Navbar from './Components/Navbar';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home></Home>
+    }
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container text-white px-14 py-8 mx-auto">
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
