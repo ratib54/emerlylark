45396205138a6146e3ed72938715cb779f68de6c
FarhanZizz <iamfarhanaziz@gmail.com>
2022-12-26 12:55:06 +0530

    added ring patterns

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import Lottie from "lottie-react";
 import animation from "../Assets/18123-developer.json";
-import rings from "../Assets/pattern-rings.svg"
 const About = () => {
 
     return (
         <section id='about' className='py-10 relative'>
             <h1 className='text-4xl md:text-5xl font-bold mt-20 '>About Me</h1>
-            <img
-                className="absolute top-20 -right-44"
-                src={rings}
-                alt=""
-                width="530"
-                height="130"
-            />
+
             <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-center">
                 <Lottie className='lg:w-2/3 md:mx-auto' animationData={animation} />
                 <p className='text-xl'>Hi I'm Farhan Aziz Mukto. I'm 18 years old. I study General Science Higher Secondary at Probortok College Chittagong. I'm a MERN stack developer. I am interested in IT and everything in its orbit. I recently began to be fascinated by web programming, e.g. developing apps and building websites and I'm Learning in this field for more than 2 years.</p>
